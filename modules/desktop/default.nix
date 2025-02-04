020e7d89e015a13aa763391e4504173a0d77370b
Sleither Reyes <sleither.reyes@live.de>
2025-02-04 20:28:07 +0100

    feat: add desktop module

---
+++ modules/desktop/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  cfg = config.slay.desktop;
+in {
+  options.slay.desktop = {
+    enable = lib.mkEnableOption "Enable desktop related options";
+  };
+
+  config = lib.mkIf cfg.enable {
+    slay.adb.enable = true;
+    slay.bootloader.enable = true;
+    slay.docker.enable = true;
+    slay.fonts.enable = true;
+    slay.home-manager.enable = true;
+    slay.plasma.enable = true;
+    slay.stylix.enable = true;
+    slay.vpn.enable = true;
+    slay.yubikey.enable = true;
+  };
+}
