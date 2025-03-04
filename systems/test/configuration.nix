69bfe4c83305d08de5e516e773aca0f7accc62cc
Sleither Reyes <s.reyes@human.de>
2025-03-05 00:27:05 +0100

    feat: add cachix

--- systems/test/configuration.nix
+++ systems/test/configuration.nix
@@ -28,10 +28,6 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
 
-  services.xserver.displayManager.sessionPackages = [
-    inputs.hyprland.packages.${pkgs.system}.default
-  ];
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
