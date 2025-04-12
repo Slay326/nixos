ca7ccf551ce69b6660a9da45179d606f2ff7d984
Sleither Reyes <sleither.reyes@gmx.de>
2025-04-12 09:41:38 +0200

    chore: revert hardcoded ssh keys

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -18,7 +18,7 @@
       commit.gpgsign = true;
       gpg.format = "ssh";
       user.signingkey = "/home/${systemConfig.user.username}/.ssh/id_ed25519_sk.pub";
-      gpg.ssh.allowedSignersFile = "/home/${systemConfig.user.username}/.ssh/allowed_signers";
+      #gpg.ssh.allowedSignersFile = "/home/${systemConfig.user.username}/.ssh/allowed_signers";
     };
 
     difftastic.enable = true;
