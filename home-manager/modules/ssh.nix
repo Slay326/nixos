1b6ebc7df5212db525d5fb34333994917b0e1c1a
Sleither Reyes <sleither.reyes@gmx.de>
2025-04-12 09:47:44 +0200

    chore: remove obsolet code

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -26,25 +26,21 @@
         hostname = "git.airshade.net";
         user = "gitlab";
         identitiesOnly = true;
-        #identityFile = "~/.ssh/id_ed25519_sk";
       };
       mercury = lib.hm.dag.entryAfter ["andromeda"] {
         hostname = "10.0.0.10";
         proxyJump = "andromeda";
         user = "root";
-        #identityFile = "~/.ssh/proxmox";
       };
       silicon = lib.hm.dag.entryAfter ["andromeda"] {
         hostname = "10.0.0.20";
         proxyJump = "andromeda";
         user = "root";
-        #identityFile = "~/.ssh/proxmox";
       };
       voyager-01 = lib.hm.dag.entryAfter ["andromeda"] {
         hostname = "10.0.0.21";
         proxyJump = "andromeda";
         user = "root";
-        #identityFile = "~/.ssh/proxmox";
       };
     };
   };
